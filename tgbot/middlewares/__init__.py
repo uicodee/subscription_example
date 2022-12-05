f1a6aa0d8187c33bf34085306ba931330ea1554c
uicodee <uicodee@gmail.com>
2022-12-05 20:16:12 +0500

    added subscription

--- tgbot/middlewares/__init__.py
+++ tgbot/middlewares/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 from sqlalchemy.orm import sessionmaker
 from .db import DbSessionMiddleware
+from .subscription import Subscription
 
 
 def register_middlewares(dp: Dispatcher, session_fabric: sessionmaker) -> None:
     dp.middleware.setup(DbSessionMiddleware(session_pool=session_fabric))
+    dp.middleware.setup(Subscription())
